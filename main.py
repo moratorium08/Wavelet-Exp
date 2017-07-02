78a506ddba89651fd4d1ffbccbe5068b6513fe9b
moratorium08 <moratorium0hiro8sanctuary@gmail.com>
2017-07-02 22:24:13 +0900

    Fixed bugs

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import pyaudio
 filename = "affection.wav"
 block = 128
 sampwidth = 2
-channels = 2
+#channels = 2
+channels = 1
 framerate = 44100
 
 dumpfile = "dump128blocks-raw"
@@ -33,19 +34,24 @@ def playback_file(filename):
 # size must be multiple of 4096
 def get_data(filename, size, shift=3):
     wf = wave.open(filename, "r")
-
     chunk = 4096
     data = wf.readframes(chunk)
     for i in range(shift - 1):
         data = wf.readframes(chunk)
-        print(len(data))
-    ret = bytes()
+    byte_data = bytes()
     for i in range(size // chunk):
-        ret = ret + wf.readframes(chunk)
+        byte_data = byte_data + wf.readframes(chunk)
+
+    ret = bytes()
+    print(len(byte_data))
+    for i in range(0, len(byte_data), 4):
+        if i % 100000 == 0:
+            print(i)
+        ret = ret + byte_data[i:i + 2]
+    print(len(ret))
     return ret
 
 def playback_data(data):
-
     p = pyaudio.PyAudio()
     stream = p.open(format=p.get_format_from_width(sampwidth),
             channels=channels,
@@ -57,27 +63,48 @@ def playback_data(data):
     stream.close()
     p.terminate()
 
-# data = get_data(filename, 4096 * block, 20)
+def bytes2vec(data):
+    ret = []
+    for i in range(0, len(data), 2):
+        x = ord(data[i + 1]) * 256 + ord(data[i])
+        ret.append(x)
+    return ret
+
+def vec2bytes(vec):
+    ret = bytes()
+    for x in vec:
+        x2 = x % 256
+        x1 = x // 256
+        ret += chr(x2) + chr(x1)
+    return ret
+
+
+#data = get_data(filename, 4096 * block, 20)
 with open(dumpfile, "rb") as f:
     data = f.read()
-playback_data(data)
+    #f.write(data)
 
+assert bytes2vec(vec2bytes([1, 2, 3])) == [1, 2, 3]
+#data = bytes2vec(data)
+#playback_data(vec2bytes(data))
 
+def upsampling(vec, size=2):
+    ret = []
+    for x in vec:
+        ret.extend([x] + [0 for i in range(size - 1)])
+    return ret
 
-# scaling vector
-def get_s_vector(p, shift):
+def downsampling(vec, size=2):
+    return vec[::size]
+
+def convolution(vec1, vec2):
     pass
 
-# wavelet vector
-def get_w_vector(p, shift):
-    pass
+assert downsampling(upsampling([1, 2, 3])) == [1, 2, 3]
 
-def inner_product(v1, v2):
-    pass
 
 def compress(filename):
     # data reading
-
 
     # Pp(u), Qp(u), Qp(u-1) ...Q1(u)のそれぞれの係数の計算
 
